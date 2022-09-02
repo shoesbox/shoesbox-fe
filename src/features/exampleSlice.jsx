1c041ab92c14c8089e37d5ca05a2224df99a2ffb
Sunny <109063966+lovysunny7@users.noreply.github.com>
2022-09-02 17:08:12 +0900

    Revert "Issue 30"

--- src/features/exampleSlice.jsx
+++ src/features/exampleSlice.jsx
@@ -1,6 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = [];
+initialState = [];
 
 export const exampleSlice = createSlice({
   name: 'example',
---
+++ src/router/routes.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Routes, Route} from 'react-router-dom'
+
+const routes = () => {
+  return (
+    <div>routes</div>
+  )
+}
+
+export default routes
