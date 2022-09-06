0fdab8a1bd4e4e063765862ab97d3d883413b3ae
hyexjun <hyebyjun@gmail.com>
2022-09-06 20:21:36 +0900

    Chore : 불필요 코드 및 파일 정리

--- src/utils/auth.jsx
+++
@@ -1,3 +0,0 @@
-export const saveToken = (token) => window.localStorage.setItem('jwt', token);
-export const getToken = () => window.localStorage.getItem('jwt');
-export const removeToken = () => window.localStorage.removeItem('jwt');
