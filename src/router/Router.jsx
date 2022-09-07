9304fb55ef2156153852f4e448159fece34c4999
hyexjun <hyebyjun@gmail.com>
2022-09-07 12:55:05 +0900

    Feat : 비로그인 시 특정 url redirect 최종 완성

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,21 +1,20 @@
-import { BrowserRouter, Route, Routes, Redirect } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 import { getCookie } from '../shared/cookie';
 import Header from '../components/Header';
 import MainPage from '../pages/MainPage';
 import DetailPage from '../pages/DetailPage';
 import WritePage from '../pages/WritePage';
 import NotFoundPage from '../pages/NotFoundPage';
-import { useEffect } from 'react';
-import { useState } from 'react';
 
 const Router = () => {
   const cookie = getCookie('accessToken');
-  const [isLoggedin, setisLoggedin] = useState(false);
+  const [isLoggedIn, setisLoggedIn] = useState(false);
   useEffect(() => {
     if (cookie !== undefined) {
-      setisLoggedin(true);
+      setisLoggedIn(true);
     } else {
-      setisLoggedin(false);
+      setisLoggedIn(false);
     }
   }, [cookie]);
 
@@ -24,12 +23,14 @@ const Router = () => {
       <Header />
       <Routes>
         <Route path="/" element={<MainPage />} />
-        {isLoggedin ? (
-          <>
-            <Route path="/detail" element={<DetailPage />} />
-            <Route path="/write" element={<WritePage />} />
-          </>
-        ) : null}
+        <Route
+          path="/detail"
+          element={isLoggedIn ? <DetailPage /> : <Navigate replace to="/" />}
+        />
+        <Route
+          path="/write"
+          element={isLoggedIn ? <WritePage /> : <Navigate replace to="/" />}
+        />
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </BrowserRouter>
