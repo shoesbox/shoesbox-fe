4f8b54c041a20fecdb23cf28497165cb47b685a5
caffesale <pglibrary80@gmail.com>
2022-09-08 13:32:31 +0900

    Fix 비동기 통신 달력에 반영
    달력에 백그라운드 이미지 출력
    
    [#40]
    
    -----------------------------------------------
    [체크리스트]
    
        제목 첫 글자는 대문자로 작성했나요?
        제목은 명령문으로 작성했나요?
        제목 끝에 마침표 금지
        제목과 본문을 한 줄 띄워 분리하기
        본문에 여러 줄의 메세지를 작성할 때는 "~"로 구분하기
    -----------------------------------------------

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -4,17 +4,18 @@ import { useEffect, useMemo, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { apis } from '../api';
 import axios from 'axios';
+import { getCookie, setCookie } from '../shared/cookie';
 
 const Calendar = () => {
+
   const memberId = 12;
+
   // 날짜 계산용 state
   const [date, setDate] = useState(new Date());
   // 달력에 그려주는 state
   const [dates, setDates] = useState([]);
-  // 달력 데이터 담아두기용
-  const [prevTray, setPrevTray] = useState();
-  const [thisTray, setThisTray] = useState({});
-  const [nextTray, setNextTray] = useState({});
+  // axios 통신용 state
+  const [ax, setAx] = useState([]);
 
   // 계산할 때 사용되지 않음, 연, 월 표시용
   const viewDate = useMemo(() => {
@@ -24,15 +25,8 @@ const Calendar = () => {
     };
   }, [date]);
 
-  // 임시 테스트용 
-  const prevMonthData = async () => {
-    const response = await axios.get('http://localhost:5001/data');
-    console.log(response?.data.content)
-    setPrevTray(response?.data.content)
-  }
-
   // 달력에 쓸 월, 일 계산용
-  const calcDate = (setPrevTray) => {
+  const calcDate = () => {
     // 지난 달 마지막 Date, 이번 달 마지막 Date
     const prevLast = new Date(date.getFullYear(), date.getMonth(), 0);
     const thisLast = new Date(date.getFullYear(), date.getMonth() + 1, 0);
@@ -49,28 +43,6 @@ const Calendar = () => {
     const thisDates = [...Array(thisLastDate + 1).keys()].slice(1);
     const nextDates = [];
 
-    // 백엔드에서 imageurl 받아오기
-    // const prevMonthData = async (memberId) => {
-    //   const response = await apis.getTargetPosts(memberId, viewDate.year, viewDate.month);
-    //   const tray = response?.data.data.content;
-    //   setPrevTray({prev: response?.data.data.content})
-    //   console.log('here12', prevTray);
-    // };
-
-    // const thisMonthData = async () => {
-    //   const response = await apis.getTargetPosts(memberId, viewDate.year, (viewDate.month+1));
-    //   const tray = response?.data.data.content;
-    //   setThisTray({this: response?.data.data.content})
-    //   console.log('here12', thisTray);
-    // };
-
-    // const nextMonthData = async () => {
-    //   const response = await apis.getTargetPosts(memberId, viewDate.year, (viewDate.month+2));
-    //   const tray = response?.data.data.content;
-    //   setNextTray({next: response?.data.data.content})
-    //   console.log('here12', nextTray);
-    // };
-    
 
     // prevDates 계산
     if (prevLastDay !== 6) {
@@ -84,41 +56,50 @@ const Calendar = () => {
       nextDates.push(i);
     }
 
-    // prevDates에 tray image데이터 돌려주기
-
-    for (let i = 0; i < prevDates.length; i++) {
-      
-    }
-    let newPrev = prevDates.reduce((arr, v) => {
-      v == prevTray?.createdDay 
-      ? arr.push({day:v, image: prevTray?.prev.thumbnailUrl})
-      : arr.push({day:v})
-      return arr;
+    // Tray 작성
+    let prevT = prevDates.reduce((arr, v) => {
+      arr.push({day: v, url:''})
+      return arr
     }, [])
-    
 
-    // thisDates에 tray image데이터 돌려주기
-    // thisDates.reduce((arr, v) => {
-    //   if(v == thisTray?.this.createdDay){
-    //     arr.push({day:v, image: thisTray?.this.thumbnailUrl})
-    //   }
-    //   else{
-    //     // arr.push({day:v})
-    //   }
-    // },[])
-    // // nextDates에 tray image데이터 돌려주기
-    // nextDates.reduce((arr, v) => {
-    //   if(v == nextDataTray?.createdDay){
-    //     arr.push({day:v, image: nextDataTray?.thumbnailUrl})
-    //   }
-    //   else{
-    //     // arr.push({day:v})
-    //   }
-    //   return arr;
-    // }, [])
+    let thisT = [];
 
+    for (let i = 0; i < thisDates.length; i++) {
+      if(thisDates[i] == ax[i]?.postId){
+        thisT.push({day:thisDates[i], url:ax[i]?.thumbnailUrl})
+      }
+      else{
+        thisT.push({day:thisDates[i], url: ''})
+      }
+    }
+
+    // let thisT = thisDates.reduce((arr, v) => {
+    //     for (let i = 0; i < thisDates.length; i++) {  
+
+    //       v == ax[i]?.postId 
+    //       ? arr.push({day:v, url:ax[i]?.thumbnailUrl})
+    //       : arr.push({day:v, url:''})
+
+    //       // if(v == ax[i]?.postId){
+    //       //   arr.push({day:v, url:ax[i]?.thumbnailUrl})
+    //       // }
+    //     }
+    //     // arr.push({day:v, url:''})
+    //     return arr
+    //     }, [])
+
+        
+        let nextT = nextDates.reduce((arr, v) => {
+          arr.push({day: v, url:''})
+          return arr
+        }, [])
     // Dates 배열 합치기
-    return prevDates.concat(thisDates, nextDates);
+    // { prevDates, thisDates, nextDates }
+    console.log("전체배열 한번 보자", prevT.concat(thisT, nextT))
+
+    
+    // return prevDates.concat(thisDates, nextDates);
+    return prevT.concat(thisT, nextT)
   };
 
   const changeMonth = (addMonth) => {
@@ -134,10 +115,19 @@ const Calendar = () => {
   };
 
   useEffect(() => {
-    setDates(calcDate());
-    prevMonthData();
-    console.log("동기성 테스트1", prevTray)
-  }, [date]);
+    axios.get('http://localhost:5001/data')
+      .then(res => res.data.content)
+      .then((data) => {
+          setAx(data)
+      })
+  }, []) 
+
+  useEffect(() => {
+    setDates(calcDate())
+
+    let print = calcDate()
+    console.log("뿌리기 위한 배열 확인", print)
+    },[ax, date])
 
   const navigate = useNavigate();
   // const [posts, setPosts] = useState([]);
@@ -186,7 +176,7 @@ const Calendar = () => {
             <button className="nav-btn" onClick={() => changeMonth(+1)}>
               &gt;
             </button>
-            {/* <Button className="nav-btn go-next">&gt;</Button> */}
+            <Button className="nav-btn go-next">&gt;</Button>
           </div>
         </div>
         <div className="main">
@@ -200,14 +190,14 @@ const Calendar = () => {
             <div className="day">토</div>
           </div>
           <div className="dates">
-            {dates.map((date, idx) => (
+            {dates.map((day, idx) => (
               <div
                 className="date"
                 key={idx}
-                style={{ background: `url(${date.image})`, backgroundSize: 'cover' }}
+                style={{ background: `url(${day.url})`, backgroundSize: 'cover' }}
                 onClick={() => navigate('/detail')}
               >
-                <span>{date}</span>
+                <span>{day.day}</span>
               </div>
             ))}
           </div>
@@ -218,3 +208,28 @@ const Calendar = () => {
 };
 
 export default Calendar;
+
+// let data = [10, 20, 30];
+// let tray = [
+//     {
+//         postId:10,
+//         url:"url",
+//     },
+//     {
+//         postId:20,
+//         url:"url2"
+//     }
+// ]
+// let image = [];
+
+
+// image = data.reduce((arr, v)=>{
+//     for (let i = 0; i < tray.length; i++) {
+//         if(v == tray[i].postId){
+//             arr.push({day:v, url:tray[i].url})
+//         }
+//     }
+//     return arra
+// },[])
+
+// console.log(image)
--- src/features/calenderSlice.jsx
+++ src/features/calenderSlice.jsx
@@ -2,3 +2,30 @@ import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { apis } from "../api";
 import axios from "axios";
 
+const initialState = {
+  prev: [],
+  this: [],
+  next: []
+};
+
+export const getCalenderThunk = createAsyncThunk(
+  '/api/calender',
+  async () => {
+      const response = await axios.get('http://localhost:5001/data')
+      return response.content
+  }
+)
+
+const calenderSlice = createSlice({
+  name: 'calender',
+  initialState: initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(getCalenderThunk.fulfilled, (state, action) => {
+      state.prev = action.payload
+    })
+  }
+})
+
+
+export default calenderSlice.reducer;
