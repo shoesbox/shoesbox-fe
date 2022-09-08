2e09575eb3e59c608340c30bed4528975e4d9289
hyexjun <hyebyjun@gmail.com>
2022-09-08 16:45:02 +0900

    Chore : 병합 대비용 백업 파일

---
+++ src/components/ModalAddFriend copy.jsx
@@ -0,0 +1,50 @@
+import './css/modaladdfriend.css';
+import { Button, Form, InputGroup, Modal } from 'react-bootstrap';
+import { BsPersonPlusFill, BsSearch } from 'react-icons/bs';
+
+const ModalAddFriend = (props) => {
+  const addMemberList = ['콩순이', 'hello'];
+  const AddMemberList = () => {
+    return (
+      <div className="addfriend-list">
+        {addMemberList.map((member, idx) => (
+          <div key={idx} className="friend-item">
+            <div>{member}님이 친구 맺기를 신청했습니다.</div>
+            <div>
+              <Button>O</Button>
+              <Button>X</Button>
+            </div>
+          </div>
+        ))}
+      </div>
+    );
+  };
+
+  return (
+    <Modal className="detail-modal" {...props} centered size="md">
+      <Modal.Header closeButton>
+        <Modal.Title>
+          <div>친구 신청</div>
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <InputGroup>
+          <InputGroup.Text className="addfriend-friend-icon">
+            <BsPersonPlusFill />
+          </InputGroup.Text>
+          <Form.Control placeholder="친구 추가" />
+          <InputGroup.Text className="addfriend-search-icon">
+            <BsSearch />
+          </InputGroup.Text>
+        </InputGroup>
+        <br />
+        <AddMemberList />
+      </Modal.Body>
+      {/* <Modal.Footer>
+        <Button onClick={props.onHide}>Close</Button>
+      </Modal.Footer> */}
+    </Modal>
+  );
+};
+
+export default ModalAddFriend;
