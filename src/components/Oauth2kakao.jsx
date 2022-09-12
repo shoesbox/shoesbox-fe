5fcfa5d35eb69442bac6f23a278258d3d2bbe5c1
caffesale <pglibrary80@gmail.com>
2022-09-12 21:43:42 +0900

    Chore 소셜로그인 카카오
    [#4]
    
    -----------------------------------------------
    [체크리스트]
    
        제목 첫 글자는 대문자로 작성했나요?
        제목은 명령문으로 작성했나요?
        제목 끝에 마침표 금지
        제목과 본문을 한 줄 띄워 분리하기
        본문에 여러 줄의 메세지를 작성할 때는 "~"로 구분하기
    -----------------------------------------------

--- src/components/Oauth2kakao.jsx
+++ src/components/Oauth2kakao.jsx
@@ -8,30 +8,37 @@ import { setCookie } from "../shared/cookie";
 const Oauth2kakao = () => {
   const navigate = useNavigate();
 
-  let code = new URL(window.location.href).searchParams.get("code");
   // console.log(code);
-  useEffect(async () => {
-    if(code){
-      const res = await apis.loginKakao(code);
-      
-      const token = res.data?.data;
-      setCookie(
-        'kakaoToken',
-        token.accessToken,
-        token.accessTokenExpireDate
-        );
-      setCookie(
-        'kakaorefreshToken',
-        token.refreshToken,
-        token.refreshTokenExpireDate
-        );
-      setCookie('memberId', token.memberId);
-      navigate('/');
+  useEffect(() => {
+    const fetchUser = async () => {
+      let code = new URL(window.location.href).searchParams.get("code");
+      console.log(code);
+
+      // if(code){
+        const res = await apis.loginKakao(code);
+        
+        const token = res.data?.data;
+        setCookie(
+          'kakaoToken',
+          token.accessToken,
+          token.accessTokenExpireDate
+          );
+          setCookie(
+            'kakaorefreshToken',
+            token.refreshToken,
+            token.refreshTokenExpireDate
+            );
+            setCookie('memberId', token.memberId);
+            navigate('/');
+      }
+
+      fetchUser();
     }
-    else{
-      alert('카카오 로그인에 실패했습니다.')
-    }
-  }, [])
+    // else{
+    //   alert('카카오 로그인에 실패했습니다.')
+    // }
+  // }
+  , [])
 
   return (
     <>
