2f26f918b98dc28f7263fc39b0884e5c806ce946
pglibrary80 <106072839+caffesale@users.noreply.github.com>
2022-09-19 15:03:09 +0900

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
- echo "> FE 배포"# 제가 본 예제에서는 S3에서 EC2로 파일을 받는 폴더를 별도로 지정한 후에 복사하여 원하는 경로에 파일을 이동하는 작업을 진행한 것 같습니다.
-# 해당 코드 출처: # <https://www.sunny-son.space/AWS/Github%20Action%20CICD/>
-# 어떤 작업을 진행하는 지는 본인이 지정하면 될 것 같습니다.
-# 저는 별도의 작업을 진행하지 않았습니다. 
- sudo cp -rf /home/ubuntu/deploy-fe/dist/* /var/www/html
+
+ echo "> FE 배포"
+ sudo cp -rf /home/share/html/build/dist/* /var/www/html
