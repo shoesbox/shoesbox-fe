f998ab8b29627f7afda5d09e1f4e56b345bff967
pglibrary80 <106072839+caffesale@users.noreply.github.com>
2022-09-20 13:56:24 +0900

    sh 파일 업데이트
    node.js 서버 실행코드

--- after-deploy.sh
+++ after-deploy.sh
@@ -4,7 +4,13 @@ REPOSITORY=/home/ubuntu/app/shoesbox-vanilla
 
 echo "> npm install"
 
-npm install express --save
+# cd $REPOSITORY
+# npm install express --save
 
-echo "> copy server.js"
+# echo "> copy server.js"
 
+# sudo cp -r server.js $REPOSITORY
+
+# echo "> node.js 배포"
+
+# sudo nohup node server.js &
