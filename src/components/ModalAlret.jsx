fd1a6da66c9b010ac5871d0394ca9975bbc68f23
hyexjun <hyebyjun@gmail.com>
2022-09-22 15:13:54 +0900

    Design : 알림 모달에 데이터 들어갈 곳 만들어두기

--- src/components/ModalAlret.jsx
+++ src/components/ModalAlret.jsx
@@ -1,11 +1,9 @@
-import './css/modaladdfriend.css';
+import './css/modalalert.css';
 import { Button, Form, Modal } from 'react-bootstrap';
 import { useState } from 'react';
 import { apis } from '../api';
 
 const ModalAlert = (props) => {
-
-
   return (
     <Modal {...props} centered size="md">
       <Modal.Header closeButton>
@@ -13,8 +11,13 @@ const ModalAlert = (props) => {
           <div>알림 🍧</div>
         </Modal.Title>
       </Modal.Header>
-      <Modal.Body>
-
+      <Modal.Body className="alert-list">
+        <div>
+          <span>어쩌구</span>님이 새로운 일기를 등록했어요!
+        </div>
+        <div>
+          <span>어쩌구</span>님이 일기에 댓글을 등록했어요!
+        </div>
       </Modal.Body>
       <Modal.Footer>
         <Button variant="secondary" onClick={props.onHide}>
