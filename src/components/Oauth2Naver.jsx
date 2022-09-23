9dcc8e851ce7b16df64be727b7d2670168ab126e
hyexjun <hyebyjun@gmail.com>
2022-09-23 20:38:06 +0900

    .

--- src/components/Oauth2Naver.jsx
+++ src/components/Oauth2Naver.jsx
@@ -6,32 +6,30 @@ import { setCookie } from '../shared/cookie';
 
 const Oauth2Naver = () => {
   const navigate = useNavigate();
-  const code = new URL(window.location.href).searchParams.get('code');
-  console.log('code', code);
 
   useEffect(() => {
     const fetchUser = async () => {
+      const code = new URL(window.location.href).searchParams.get('code');
+      console.log('code', code);
       if (code) {
-        apis
-          .loginNaver(code)
-          .then((res) => res.data?.data)
-          .then((token) => {
-            setCookie(
-              'accessToken',
-              token.accessToken,
-              token.accessTokenExpireDate
-            );
-            setCookie(
-              'refreshToken',
-              token.refreshToken,
-              token.refreshTokenExpireDate
-            );
-            setCookie('memberId', token.memberId);
-            setCookie('email', token.email);
-            setCookie('nickname', token.nickname);
-          });
+        apis.loginNaver(code).then((res) => {
+          const token = res.data?.data;
+          setCookie(
+            'accessToken',
+            token.accessToken,
+            token.accessTokenExpireDate
+          );
+          setCookie(
+            'refreshToken',
+            token.refreshToken,
+            token.refreshTokenExpireDate
+          );
+          setCookie('memberId', token.memberId);
+          setCookie('email', token.email);
+          setCookie('nickname', token.nickname);
+        });
       } else {
-        alert('네이버 로그인에 실패했습니다.');
+        alert('카카오 로그인에 실패했습니다.');
       }
     };
     fetchUser();
@@ -40,6 +38,38 @@ const Oauth2Naver = () => {
       navigate('/');
     }, 1000);
   }, []);
+
+  // useEffect(() => {
+  //   const fetchUser = async () => {
+  //     if (code) {
+  //       apis
+  //         .loginNaver(code)
+  //         .then((res) => res.data?.data)
+  //         .then((token) => {
+  //           setCookie(
+  //             'accessToken',
+  //             token.accessToken,
+  //             token.accessTokenExpireDate
+  //           );
+  //           setCookie(
+  //             'refreshToken',
+  //             token.refreshToken,
+  //             token.refreshTokenExpireDate
+  //           );
+  //           setCookie('memberId', token.memberId);
+  //           setCookie('email', token.email);
+  //           setCookie('nickname', token.nickname);
+  //         });
+  //     } else {
+  //       alert('네이버 로그인에 실패했습니다.');
+  //     }
+  //   };
+  //   fetchUser();
+
+  //   setTimeout(() => {
+  //     navigate('/');
+  //   }, 1000);
+  // }, []);
 
   return (
     <div className="loading-container">
