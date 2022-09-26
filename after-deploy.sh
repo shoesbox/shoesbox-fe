db7ec2336c8f1c37e1f5c6d0be3eb32d83d11afc
pglibrary80 <106072839+caffesale@users.noreply.github.com>
2022-09-26 15:02:40 +0900

    Update after-deploy.sh

--- after-deploy.sh
+++ after-deploy.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-REPOSITORY=/home/ubuntu/app
+# REPOSITORY=/home/ubuntu/app
 
-echo "> pm2 종료"
-cd $REPOSITORY
-sudo pm2 kill
+# echo "> pm2 종료"
+# cd $REPOSITORY
+# sudo pm2 kill
 
-echo "> pm2 실행"
-cd $REPOSITORY
-sudo pm2 start server.js
+# echo "> pm2 실행"
+# cd $REPOSITORY
+# sudo pm2 start server.js
 
-echo "> pm2 실행 확인"
-sudo pm2 list
+# echo "> pm2 실행 확인"
+# sudo pm2 list
