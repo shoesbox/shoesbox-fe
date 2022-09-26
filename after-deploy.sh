695cacf24a498917597a8ad0ef5189a1ca4b8696
pglibrary80 <106072839+caffesale@users.noreply.github.com>
2022-09-26 15:07:31 +0900

    Update after-deploy.sh

--- after-deploy.sh
+++ after-deploy.sh
@@ -2,13 +2,13 @@
 
 # REPOSITORY=/home/ubuntu/app
 
-# echo "> pm2 종료"
-# cd $REPOSITORY
-# sudo pm2 kill
+echo "> pm2 종료"
+cd $REPOSITORY
+sudo /home/ubuntu/.nvm/versions/node/v18.9.1/bin/pm2 kill
 
-# echo "> pm2 실행"
-# cd $REPOSITORY
-# sudo pm2 start server.js
+echo "> pm2 실행"
+cd $REPOSITORY
+sudo /home/ubuntu/.nvm/versions/node/v18.9.1/bin/pm2 start server.js
 
-# echo "> pm2 실행 확인"
-# sudo pm2 list
+echo "> pm2 실행 확인"
+sudo /home/ubuntu/.nvm/versions/node/v18.9.1/bin/pm2 list
